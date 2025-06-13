6736d5b618ec9a9e52120368cc7fb493bd5602a3
n19k6 <46622208+n19k6@users.noreply.github.com>
2025-06-13 10:36:50 +0200

    wip

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# github_actions
+# actions
--- scripts/script_v1.py
+++ scripts/script_v1.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 from time import asctime, localtime
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+
+
 
 if __name__ == "__main__":
     #print(2+40)
     #print("Hello, World!")
     print(asctime(localtime()))
+    berlin_time = datetime.now(ZoneInfo("Europe/Berlin"))
+    print(berlin_time.strftime("%Y-%m-%d %H:%M:%S"))
