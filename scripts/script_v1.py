d27dfefbe65234569cc00419d088749cbf8c7e40
n19k6 <46622208+n19k6@users.noreply.github.com>
2025-06-23 13:23:56 +0200

    Update script_v1.py

--- scripts/script_v1.py
+++ scripts/script_v1.py
@@ -9,6 +9,6 @@ from zoneinfo import ZoneInfo
 if __name__ == "__main__":
     #print(2+40)
     #print("Hello, World!")
-    print(asctime(localtime()))
+    #print(asctime(localtime()))
     berlin_time = datetime.now(ZoneInfo("Europe/Berlin"))
     print(berlin_time.strftime("%Y-%m-%d %H:%M:%S"))
